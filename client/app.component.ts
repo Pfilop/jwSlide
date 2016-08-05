6932276f4608c0d92e466752fc5fe8214c7e4fec
Pfilop <philipp@tavra.de>
2016-08-05 13:05:25 +0200

    step 4

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core';
-import { Mongo } from 'meteor/mongo';
-import { Parties }   from '../both/collections/parties.collection';
 
 import template from './app.component.html';
 
 @Component({
-    selector: 'app',
-    template
+	selector: 'app',
+	template
 })
-export class AppComponent {
-    parties: Mongo.Cursor<any>;
-
-    constructor() {
-        this.parties = Parties.find();
-    }
-}
+export class AppComponent {}
